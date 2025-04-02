496c34f1832df483d3cc9b81486f1c508a166317
Ymilak2018 <yashmilak20@gmail.com>
2025-04-02 18:23:57 +0530

    Setup Done

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import find_packages, setup
+from typing import List
+
+
+HYPHEN_E = '-e .'
+
+def get_requirements(filepath):
+
+    requirements = []
+
+    with open(filepath) as obj:
+        requirements = obj.readlines()
+        requirements = [r.replace("\n", '') for r in requirements]
+
+    if HYPHEN_E in requirements:
+        requirements.remove(HYPHEN_E)
+
+    return requirements
 
 setup(
 
@@ -7,6 +24,6 @@ version = "0.0.1",
 author = "Yash Milak",
 author_email = "yashmilak20@gmail.com",
 packages = find_packages(),
-install_requires = ['numpy', 'pandas', 'seaborn']
+install_requires = get_requirements("requirements.txt")
 
 )
