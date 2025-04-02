263ea6c85227a365304d1a9553ee210cd6b83754
Ymilak2018 <yashmilak20@gmail.com>
2025-04-02 18:06:27 +0530

    commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+
+name="ml project",
+version = "0.0.1",
+author = "Yash Milak",
+author_email = "yashmilak20@gmail.com",
+packages = find_packages(),
+install_requires = ['numpy', 'pandas', 'seaborn']
+
+)
